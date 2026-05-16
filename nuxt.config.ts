56c81ce982778d424bfe44a349248964a9b3b0ae
DavidUnicor <sr.sheisyt@gmail.com>
2026-05-16 14:37:46 -0500

    cambio de icono

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,7 @@ export default defineNuxtConfig({
     head: {
       title: 'OVA: Teorema de Pitágoras',
       link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap' }
